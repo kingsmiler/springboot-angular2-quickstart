3946de003caeadc2674be30df4a5eb4287e370e5
kingsmiler <kingsmiler@163.com>
2016-08-22 23:15:09 +0800

    initialize springboot angular2 quick start example

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # springboot-angular2-quickstart
+
+run "gradle bootRun" to launch the gradle project of spring boot.
---
+++ src/main/java/org/xman/angular2/Angular2Application.java
@@ -0,0 +1,13 @@
+package org.xman.angular2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Angular2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Angular2Application.class, args);
+    }
+
+}
