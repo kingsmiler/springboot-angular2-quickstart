898410fa07bcc62e6ff4331f2e28ea9d51b2b67f
kingsmiler <kingsmiler@163.com>
2016-08-24 16:14:30 +0800

    use HashLocationStrategy to fix url problem, because springboot just only map / .

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -1,11 +1,16 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {HashLocationStrategy, LocationStrategy} from "@angular/common";
 
-import { AppComponent }  from './app.component';
+import {AppComponent} from "./app.component";
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+    imports: [BrowserModule],
+    declarations: [AppComponent],
+    providers: [
+        {provide: LocationStrategy, useClass: HashLocationStrategy}
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
